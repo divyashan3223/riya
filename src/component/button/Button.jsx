fdfdae9c920400b1366b6169f42d7a364a01f21d
divyashan3223 <divyashan3223@gmail.com>
2025-05-16 12:09:52 +0530

    external css

--- src/component/button/Button.jsx
+++ src/component/button/Button.jsx
@@ -1,14 +1,17 @@
+import { Fragment } from "react/jsx-runtime";
+import dog from "../../assests/dog.webp";
+import "./button.css";
 export const Button = () => {
-  return <button>submit</button>;
+  return <button>download my dog image</button>;
 };
 
 const Heading = () => {
   return (
-    <h1>
-      <Button/>
-      i'm a heading
+    <div className="container">
+      <h1 id="head" className="head">this is my dog</h1>
+      <img src={dog} alt="" />
       <Button />
-    </h1>
+    </div>
   );
 };
 
