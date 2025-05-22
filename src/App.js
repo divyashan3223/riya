7555b90fa80c8f0d6d66d6b5ecab4d1140bbbfeb
divyashan3223 <divyashan3223@gmail.com>
2025-05-22 12:52:22 +0530

    external css

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ function App() {
 }
   return (
     <div className="App">
-      <Button content={"Login"} bgClr={"blue"} />
-      <Button content={"Submit"}  bgClr={"red"}/>
-      <Button  padding={"10px 20px"} content={"click"} clk={handleClick}/>
-      <Button content={"Add to cart"} border={"none"} clr={"navy"} />
+      <Button content={"Login"}  clsName={"login"} />
+      <Button content={"Submit"}  clsName={"sub"}/>
+      <Button  content={"click"} clk={handleClick}/>
+      <Button content={"Add to cart"} />
     </div>
   );
 }
--- src/component/button/Button.jsx
+++ src/component/button/Button.jsx
@@ -1,17 +1,9 @@
-const Button = (prop) => {
-  let design = {
-    backgroundColor: prop.bgClr,
-    padding: prop.padding,
-    border: prop.border,
-    color: prop.clr,
-  };
-  return (
-    <div>
-      <button onClick={prop.clk} style={design}>
-        {content}
-      </button>
-    </div>
-  );
-};
+import './button.css'
 
-export default Button;
+const Button = ({content,clsName}) => {
+  return (
+<button className={clsName} >{content}</button>
+  )
+}
+
+export default Button
