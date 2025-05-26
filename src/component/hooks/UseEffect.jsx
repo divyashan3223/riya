f62e7f08f269db80b098635444317a89c646a355
divyashan3223 <divyashan3223@gmail.com>
2025-05-26 13:05:52 +0530

    usign fetch method in useEffect hook

--- src/component/hooks/UseEffect.jsx
+++ src/component/hooks/UseEffect.jsx
@@ -1,38 +1,20 @@
-import { useEffect, useState } from "react";
+// fetch('api')
+//
 
-// !useEffect
-// useEffect(()=>{},[count1])
-// all state change rerender ()=>{}
-//  initial stage rerender  []
-// state change only change ()=>{},[state,prop]
+import { useEffect } from "react";
+import { useState } from "react";
 
 const UseEffect = () => {
-  const [count1, setCount1] = useState(10);
-  const [count2, setCount2] = useState(15);
-  const [count3, setCount3] = useState(20);
 
+    const[title,setTitle]=useState('')
   useEffect(() => {
-    return console.log("hey im runing backside");
-  },[count2]);
-  return (
-    <div>
-      <h1>count1:{count1}</h1>
-      <button onClick={() => setCount1((c) => c + 1)}>+</button>
-      <button onClick={() => setCount1((c) => c - 1)}>-</button>
-      <hr />
-      <hr />
-      <h1>count2:{count2}</h1>
-      <button onClick={() => setCount2((c) => c + 1)}>+</button>
-      <button onClick={() => setCount2((c) => c - 1)}>-</button>
-      <hr />
-      <hr />
-      <h1>count3:{count3}</h1>
-      <button onClick={() => setCount3((c) => c + 1)}>+</button>
-      <button onClick={() => setCount3((c) => c - 1)}>-</button>
-      <hr />
-      <hr />
-    </div>
-  );
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => data.map((item) => setTitle(item.title)));
+  });
+  return <div>
+    <h1>title{title}</h1>
+  </div>;
 };
 
 export default UseEffect;
